4f639077a93fedcd2fbeb19f180f55cf90d0a753
Jackie <jtran2009a@gmail.com>
2021-01-14 23:31:19 -0500

    Changed navbar design

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,42 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import SearchIcon from '@material-ui/icons/Search';
+import StarIcon from '@material-ui/icons/Star';
 
 const Navbar: React.FC = () => {
-    return(
-        <Container>
-            <AccountIcon />
-            <Title>Shoppies</Title>
-            <SearchBarIcon />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Title>Shoppies</Title>
+      <Star />
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #3F424F;
-    padding: 3%;
-`;
-
-const AccountIcon = styled(AccountCircleIcon)`
-    font-size: 3rem;
-    color: #BEE3DB;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #3f424f;
+  padding: 3%;
 `;
 
 const Title = styled.h1`
-    margin: 0;
-    color: #FAF9F9;
-    font-size: 2rem;
+  margin: 0;
+  color: #faf9f9;
+  font-size: 2rem;
 `;
 
-const SearchBarIcon = styled(SearchIcon)`
-    font-size: 3rem;
-    color: #BEE3DB;
+const Star = styled(StarIcon)`
+    color: white;
+    margin-right: 3%;
+    position: absolute;
+    right: 0;
 `;
-
-
 
 export default Navbar;
